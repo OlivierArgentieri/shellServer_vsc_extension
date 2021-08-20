998b46502081111e2785f41075b5665b8e755e55
OlivierArgentieri <olivierargentieri@gmail.com>
2021-08-20 17:34:41 +0200

    first test ok

--- first-addons/out/extension.js
+++ first-addons/out/extension.js
@@ -13,36 +13,27 @@ exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-const telnet_client_1 = require("telnet-client");
-const params = {
-    host: '127.0.0.1',
-    port: 2017,
-    shellPrompt: '/ #',
-    timeout: 1500
-};
+const net = require('net');
+var client = new net.Socket();
+client.setEncoding('utf8');
 // send to async command to create cnx and send omd
-function init() {
+function init(text) {
     return __awaiter(this, void 0, void 0, function* () {
-        const _con = new telnet_client_1.default();
-        try {
-            yield _con.connect(params);
-            yield _con.send("print('Aaaaa') #test ");
-        }
-        catch (err) {
-            console.log(err);
-        }
+        client.connect(2017, '127.0.0.1', function () {
+            console.log('Connected');
+            client.write(text);
+            client.close();
+        });
+        client.on('close', () => {
+            console.log("CLOSED");
+        });
     });
 }
-// send to async command to create cnx and send omd
 function send_to(text) {
     return __awaiter(this, void 0, void 0, function* () {
-        const _con = new telnet_client_1.default();
-        try {
-            yield _con.connect(params);
-        }
-        catch (err) {
-            console.log(err);
-        }
+        console.log(text);
+        client.write('print("deux")');
+        client.write(Buffer.from(text, 'utf-8').toString());
     });
 }
 // this method is called when your extension is activated
@@ -54,21 +45,28 @@ function activate(context) {
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('first-addons.helloWorld', () => {
-        init()
-            .then((a) => console.log("ok"))
+        init("")
+            .then((a) => console.log("ok initialize cnx"))
             .catch((err) => console.log(`err : ${err}`));
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+    });
+    let sendToDisposable = vscode.commands.registerCommand('first-addons.sendto', () => {
         var editor = vscode.window.activeTextEditor;
         if (!editor) {
             return; // No open text editor
         }
         var selection = editor.selection;
         var text = editor.document.getText(selection);
-        console.log(text);
+        init(text)
+            .then(() => console.log("ok"))
+            .catch((err) => console.log(`err : ${err}`));
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
         vscode.window.showInformationMessage(text);
     });
     context.subscriptions.push(disposable);
+    context.subscriptions.push(sendToDisposable);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
--- first-addons/src/extension.ts
+++ first-addons/src/extension.ts
@@ -1,41 +1,34 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import Telnet from 'telnet-client';
+const net = require('net')
 
-const params = {
-	host: '127.0.0.1',
-	port: 2017,
-	shellPrompt: '/ #',
-	timeout: 1500
-}
+var client = new net.Socket();
+
+client.setEncoding('utf8');
 
 // send to async command to create cnx and send omd
-async function init() {
-	const _con = new Telnet()
-	try {
-		await _con.connect(params)
-		await _con.send("print('Aaaaa') #test ")
-
-	} catch(err){
-		console.log(err)
-	}
+async function init(text:string) {
+	client.connect(2017, '127.0.0.1', function () {
+		console.log('Connected');
+		client.write(text);
+		client.close()
+	});
+	client.on('close', () => {
+		console.log("CLOSED")
+	})
 }
 
-// send to async command to create cnx and send omd
 async function send_to(text: string) {
-	const _con = new Telnet()
-	try {
-		await _con.connect(params)
-	} catch(err){
-		console.log(err)
-	}
+	console.log(text);
+	client.write('print("deux")');
+	client.write(Buffer.from(text, 'utf-8').toString());	
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 
@@ -43,17 +36,26 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('first-addons.helloWorld', () => {
-		init()
-		.then((a) => console.log("ok"))
-		.catch((err) => console.log(`err : ${err}`))
+		init("")
+			.then((a) => console.log("ok initialize cnx"))
+			.catch((err) => console.log(`err : ${err}`))
+
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+	});
+
+	let sendToDisposable = vscode.commands.registerCommand('first-addons.sendto', () => {
 		var editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			return; // No open text editor
 		}
-	
+
 		var selection = editor.selection;
 		var text = editor.document.getText(selection);
-		console.log(text);		
+
+		init(text)
+			.then(() => console.log("ok"))
+			.catch((err) => console.log(`err : ${err}`))
 
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
@@ -61,7 +63,8 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(sendToDisposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
